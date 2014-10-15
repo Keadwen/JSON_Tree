09035efb1ecae873c0044f432c10c5305ac769ed
jakubm <cubonek@gmail.com>
2014-10-15 16:41:50 +0100

    Working version of a JSON data collector. TODO: Verify the UnitTests.

--- json_collector.py
+++ json_collector.py
@@ -1,7 +1,7 @@
 # Module json_collector - locate the data inside JSON in a faster and easier way.
-from json import *
-from os import *
-from re import match
+import os
+import re
+import json
 
 __author__ = 'Jakub Mlynarczyk (jkbmlynarczyk@gmail.com)'
 
@@ -14,33 +14,52 @@ class JsonCollector(object):
   the location of file(s) and the tree from which we want to collect data.
 
   Args:
-    json_location (str): a location of the JSON file(s).
+    json_location (str): a location of the JSON file.
     tree_word (tuple): an object with values to search for.
     json_files (dict): contains all open JSON files (k: filename, v: object)
   """
+
   def __init__(self, json_location, tree_word):
     self.json_location = json_location
     self.tree_word = tree_word
-    self.json_files = dict()
+    self.json_data = {}
+    
+    self.OpenJson()
+    self.return_data = self.FindTree(self.json_data, {})
 
   def OpenJson(self):
-  """Checks the location of JSONs file and opens all of them."""
-  try:
-    os.chdir(json_location)  # Change to requested location and get files list.
-    f_list = os.listdir('./')
-  except OSError:
-    return {}
+    """Checks the location of JSONs file and reads all of them."""
+    try:
+      x = open(self.json_location)
+      self.json_data = json.load(x)
+      x.close()
+    except IOError:
+      return {}
 
-  # Open only json files.
-  for f in f_list:  
-    if re.match('.*\.json$', f):
-      self.json_files[f] = open('./'+f)
+  def FindTree(self, dict_in, dict_out):
+    """Return dict value under requested subtrees.
+
+    Method provides recursive search to collect all key:value pairs inside
+    a nested dict that can contain list and dicts as values.
+
+    Args:
+      dict_in (dict): an object with a dict to scan.
+      dict_out (dict): an object with all key:values that where nested
+
+    Returns the value of the key that has been requested by a user.
+    If a key does not exist in the JSON file, returns an empty dict object.
+    """
+    for key, value in dict_in.iteritems():
+      dict_out[key] = value             # Append the dict with a nested k:v
+      if isinstance(value, dict):       
+        self.FindTree(value, dict_out)  # Dict as a value, recursive search.
+      elif isinstance(value, list):     # List as a value.
+        for i in value:
+          self.FindTree(i, dict_out)    # Recursive search for each element.
+
+    return dict_out.get(self.tree_word, {})  # Return only requested key.
 
 
-  # create a dict of objects (key: file_name, value: open_object)
-  # return the dict
-  self.json_files = json_files
-
-
-  def FindTree(self):
-  """Return dict all values under requested location."""
+if __name__ == '__main__':
+  test = JsonCollector('/Users/jakubm/github/JSON_Tree/test.json', 'tree 1')
+  print test.return_data
